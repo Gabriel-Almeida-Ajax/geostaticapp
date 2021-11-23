ab4de3f3b3a9f73c854ccec56352601a1c4ae13a
GABRIEL SANTOS LIMA <lima.gabriel@LELLO.CORP>
2021-11-23 18:07:42 -0300

    that is a finily

--- pages/api/regions.ts
+++ pages/api/regions.ts
@@ -10,7 +10,7 @@ export default function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
 ) {
-    axios.get(`https://servicodados.ibge.gov.br/api/v3/malhas/estados/${35}?formato=application/vnd.geo+json&qualidade=intermediaria&intrarregiao=municipio`)
+    axios.get(`https://servicodados.ibge.gov.br/api/v3/malhas/estados/${req.query.id}?formato=application/vnd.geo+json&qualidade=intermediaria&intrarregiao=municipio`)
         .then(resp => {
             res.status(200).json({ data: resp.data })
         })
